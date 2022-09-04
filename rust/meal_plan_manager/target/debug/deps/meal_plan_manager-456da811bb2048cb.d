718cda52db76bb51bd0f47c81e2ccb0443ce2ac5
Thomas Kieß <thomas.kiess@gmx.de>
2022-09-04 16:14:04 +0200

    Added rust folder with inital project

---
+++ rust/meal_plan_manager/target/debug/deps/meal_plan_manager-456da811bb2048cb.d
@@ -0,0 +1,5 @@
+/home/tommi/src/github/playground/rust/meal_plan_manager/target/debug/deps/meal_plan_manager-456da811bb2048cb: src/main.rs
+
+/home/tommi/src/github/playground/rust/meal_plan_manager/target/debug/deps/meal_plan_manager-456da811bb2048cb.d: src/main.rs
+
+src/main.rs:
---
+++ rust/meal_plan_manager/target/debug/meal_plan_manager.d
@@ -0,0 +1,1 @@
+/home/tommi/src/github/playground/rust/meal_plan_manager/target/debug/meal_plan_manager: /home/tommi/src/github/playground/rust/meal_plan_manager/src/main.rs
